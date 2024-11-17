d541bf7afb3a845d2cc235140fcdff801c40b215
cleiton524 <quioratocleiton53@gmail.com>
2024-11-17 03:17:45 -0300

    admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +20,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//rota admin
+route::get('Admin/dashboard',[AdminController::class, 'dashboard'])->middleware('auth')->name('admin.dashboard');
+
+//rota vendedor
+route::get('vendor/dashboard',[VendorController::class, 'dashboard'])->middleware('auth')->name('vendor.dashboard');
